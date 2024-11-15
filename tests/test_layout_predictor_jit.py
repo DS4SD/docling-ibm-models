7a76ba9950e84a4373a6f9a96a60204dfe0a0bdf
Nikos Livathinos <nli@zurich.ibm.com>
2024-11-15 07:53:17 +0100

    chore(tests): Name the test for the JIT LayoutPredictor as test_layout_predictor_jit.py
    Signed-off-by: Nikos Livathinos <nli@zurich.ibm.com>

---
+++ tests/test_layout_predictor_jit.py
@@ -0,0 +1,89 @@
+#
+# Copyright IBM Corp. 2024 - 2024
+# SPDX-License-Identifier: MIT
+#
+import os
+
+import numpy as np
+import pytest
+from PIL import Image
+
+from huggingface_hub import snapshot_download
+
+import docling_ibm_models.layoutmodel.layout_predictor_jit as lp
+from docling_ibm_models.layoutmodel.layout_predictor_jit import LayoutPredictor
+
+
+@pytest.fixture(scope="module")
+def init() -> dict:
+    r"""
+    Initialize the testing environment
+    """
+    # This config is missing the keys: "artifact_path", "info1.torch_file", "info2.torch_file"
+    init = {
+        "num_threads": 1,
+        "test_imgs": [
+            "tests/test_data/samples/ADS.2007.page_123.png",
+        ],
+        "info1": {
+            "device": "cpu",
+            "image_size": 640,
+            "threshold": 0.6,
+        },
+        "pred_bboxes": 9,
+    }
+
+    # Download models from HF
+    download_path = snapshot_download(repo_id="ds4sd/docling-models")
+    artifact_path = os.path.join(download_path, "model_artifacts/layout/beehive_v0.0.5_pt")
+
+    # Add the missing config keys
+    init["artifact_path"] = artifact_path
+    init["info1"]["torch_file"] = os.path.join(artifact_path, lp.MODEL_CHECKPOINT_FN)
+
+    return init
+
+
+def test_layoutpredictor(init: dict):
+    r"""
+    Unit test for the LayoutPredictor
+    """
+    # Initialize LayoutPredictor with envvars
+    os.environ["TORCH_DEVICE"] = "cpu"
+    #os.environ["OMP_NUM_THREADS"] = "2"
+    lpredictor = LayoutPredictor(init["artifact_path"])
+    #assert init["info1"] == lpredictor.info()
+
+
+    # Unsupported input image
+    is_exception = False
+    try:
+        for pred in lpredictor.predict("wrong"):
+            pass
+    except TypeError:
+        is_exception = True
+    assert is_exception
+
+    # Predict on the test image
+    for img_fn in init["test_imgs"]:
+        with Image.open(img_fn) as img:
+            w, h = img.size
+            # Load images as PIL objects
+            for i, pred in enumerate(lpredictor.predict(img)):
+                print("PIL pred: {}".format(pred))
+                assert pred["l"] >= 0 and pred["l"] <= w
+                assert pred["t"] >= 0 and pred["t"] <= h
+                assert pred["r"] >= 0 and pred["r"] <= w
+                assert pred["b"] >= 0 and pred["b"] <= h
+
+            assert i + 1 == init["pred_bboxes"]
+
+            # Load images as numpy arrays
+            np_arr = np.asarray(img)
+            for i, pred in enumerate(lpredictor.predict(np_arr)):
+                print("numpy pred: {}".format(pred))
+                assert pred["l"] >= 0 and pred["l"] <= w
+                assert pred["t"] >= 0 and pred["t"] <= h
+                assert pred["r"] >= 0 and pred["r"] <= w
+                assert pred["b"] >= 0 and pred["b"] <= h
+            assert i + 1 == init["pred_bboxes"]
