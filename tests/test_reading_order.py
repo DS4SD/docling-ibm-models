e05a4b1b8e15bd530ad5c64bde7641c012f7f8c1
Peter Staar <taa@zurich.ibm.com>
2025-02-05 16:29:37 +0100

    updated the tests with reading-order on docling-dpbench
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- tests/test_reading_order.py
+++ tests/test_reading_order.py
@@ -32,100 +32,6 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-
-@pytest.fixture(scope="module")
-def init() -> dict:
-    r"""
-    Initialize the testing environment
-    """
-    # This config is missing the keys: "artifact_path", "info1.torch_file", "info2.torch_file"
-    init = {
-        "num_threads": 1,
-        "test_imgs": sorted(glob.glob("tests/test_data/samples/*.png")),
-        "info1": {
-            "use_cpu_only": True,
-            "image_size": 640,
-            "threshold": 0.6,
-        },
-        "info2": {
-            "use_cpu_only": True,
-            "image_size": 640,
-            "threshold": 0.6,
-        },
-        "pred_bboxes": 9,
-    }
-    
-    # Download models from HF
-    download_path = snapshot_download(repo_id="ds4sd/docling-models")
-    artifact_path = os.path.join(download_path, "model_artifacts/layout/beehive_v0.0.5_pt")
-
-    # Add the missing config keys
-    init["artifact_path"] = artifact_path
-    init["info1"]["torch_file"] = os.path.join(artifact_path, lp.MODEL_CHECKPOINT_FN)
-    init["info2"]["torch_file"] = os.path.join(artifact_path, lp.MODEL_CHECKPOINT_FN)
-    
-    return init
-
-def _test_readingorder(init: dict):
-    r"""
-    Unit test for the LayoutPredictor
-    """
-    # Initialize LayoutPredictor with envvars
-    os.environ["USE_CPU_ONLY"] = ""
-    os.environ["OMP_NUM_THREADS"] = "2"
-    lpredictor = LayoutPredictor(init["artifact_path"])
-    assert init["info1"] == lpredictor.info()
-
-    # Initialize LayoutPredictor with optional parameters
-    lpredictor = LayoutPredictor(
-        init["artifact_path"], use_cpu_only=True
-    )
-    assert init["info2"] == lpredictor.info()
-
-    # Unsupported input image
-    is_exception = False
-    try:
-        for pred in lpredictor.predict("wrong"):
-            pass
-    except TypeError:
-        is_exception = True
-    assert is_exception
-
-    # Init the reading-order model
-    romodel = ReadingOrderPredictor()
-    
-    # Predict on the test image
-    for img_fn in init["test_imgs"]:
-        print(img_fn)
-        
-        with Image.open(img_fn) as img:
-            pred_layout=[]
-
-            # Load images as PIL objects
-            for i, pred in enumerate(lpredictor.predict(img)):                
-                pred_layout.append({
-                    "label": pred["label"],
-                    "t": pred["t"].item(),
-                    "b": pred["b"].item(),
-                    "l": pred["l"].item(),
-                    "r": pred["r"].item(),
-                })
-            print(json.dumps(pred_layout, indent=2))
-
-            page_elements = []
-            for cid, item in enumerate(pred_layout):
-                page_elements.append(PageElement(cid=cid, pid=0,
-                                                 x0=item["l"], y0=item["r"],
-                                                 x1=item["b"], y1=item["t"],
-                                                 label=item["label"]))
-
-            print(page_elements)
-                
-            ordered_elements = romodel.predict_page(page_elements)
-
-            print(ordered_elements)
-
-
 def rank_array(arr):
     """Compute ranks, resolving ties by averaging."""
     sorted_indices = sorted(range(len(arr)), key=lambda i: arr[i])  # Sort indices
@@ -157,20 +63,11 @@ def spearman_rank_correlation(arr1, arr2):
     rho = 1 - (6 * d_squared_sum) / (n * (n**2 - 1))
     
     return rho
-
             
 def test_readingorder():
 
     # Init the reading-order model
     romodel = ReadingOrderPredictor()
-
-    """
-    filenames = sorted(glob.glob("/Users/taa/Documents/projects/docling-eval/benchmarks/DPBench-annotations-v03/json_annotations/*.json"))
-    print(f"#-filenames: {len(filenames)}")
-    
-    for filename in filenames:
-        true_doc = DoclingDocument.load_from_json(filename=filename)
-    """
 
     ds = load_dataset("ds4sd/docling-dpbench")
     for row in ds["test"]:
@@ -224,6 +121,16 @@ def test_readingorder():
         score = spearman_rank_correlation(true_cids, pred_cids)
 
         filename = row["document_id"]
-        print(f"{os.path.basename(filename)}: {score}")
-        
+
+        # Identify special cases ...
+        if filename in ["doc_906d54a21ef3c7bfac03f4bb613b0c79ef32fdf81b362450c79e98a96f88708a_page_000001.png",
+                        "doc_2cd17a32ee330a239e19c915738df0c27e8ec3635a60a7e16e2a0cf3868d4af3_page_000001.png",
+                        "doc_bcb3dafc35b5e7476fd1b9cd6eccf5eeef936cd5b13ad846a4943f1e7797f4e9_page_000001.png",
+                        "doc_a0edae1fa147c7bb78ebc493743a68ba4372b5ead31f2a2b146c35119462379e_page_000001.png",
+                        "doc_94ba5468fcb6277721947697048846dc0d0551296be3b45f5918ab857d21dcc7_page_000001.png",
+                        "doc_cbb4a13ffd01d9f777fdb939451d6a21cea1b869ee50d79581451e3601df9ec8_page_000001.png"]:
+            # print(f"{os.path.basename(filename)}: {score}")
+            assert score>=0.60, f"reading-order score={score}>0.60"            
+        else:
+            assert score>=0.90, f"reading-order score={score}>0.90"
             
